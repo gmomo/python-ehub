8abef69e54f72c0c729c1c6a17e5d5497a112899
Grey Hill <greyhill@uvic.ca>
2017-05-10 15:58:59 -0700

    Replace config object with yaml settings
    YAML is a format for settings files. It is intended to be used for
    human-readable configutations. In this case, it only specifies the input
    file, the output file, and the solver with its options.
    
    Remove line from config.py

--- cli.py
+++ cli.py
@@ -1,39 +1,15 @@
-import argparse
 from ehub_model import EHubModel
-from config import Config
-
-
-def parse_args():
-    parser = argparse.ArgumentParser(
-        description="Finds the optimal solution to a energy hub model.")
-    parser.add_argument("--input_file",
-                        default="General_input.xlsx",
-                        help="The Excel 2003 file used as input into the "
-                        "model.")
-    parser.add_argument("--output_file",
-                        default="output.txt",
-                        help="The text file to save the results of the "
-                        "solution.")
-    parser.add_argument("--solver",
-                        required=True,
-                        help="The solver to use for solving the model. The "
-                        "solver must already be installed on the system.")
-
-    return parser.parse_args()
+from config import settings
 
 
 def main():
-    args = parse_args()
+    model = EHubModel(excel=settings["input_file"])
 
-    config = Config(args)
-
-    model = EHubModel(excel=config.input_file)
-
-    results = model.solve(config.solver, config.solver_options)
+    results = model.solve()
 
     print(results)
 
-    with open(config.output_file, 'w') as f:
+    with open(settings["output_file"], 'w') as f:
         f.write(str(results))
 
 
