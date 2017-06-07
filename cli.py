7460a07a13ce7fe38ea26610c1b76261d3da1c00
Grey Hill <greyhill@uvic.ca>
2017-06-07 11:21:42 -0700

    Update cli to new response format

--- cli.py
+++ cli.py
@@ -2,73 +2,40 @@ from collections import OrderedDict
 from contextlib import redirect_stdout
 
 import numpy as np
-from pyomo.core.base import Var
 
 from ehub_model import EHubModel
 from config import settings
 
 
-def to_matrix(var):
-    # Determine the dimension of the matrix
-    try:
-        dim = len(list(var.keys())[0])
-    except TypeError:
-        dim = 1
+def pretty_print(results):
+    np.set_printoptions(linewidth=1000, suppress=True)
 
-    if dim == 1:
-        return list(var.values())
-    elif dim == 2:
-        keys = var.keys()
+    version = results['version']
+    solver = results['solver']
 
-        length = (max(keys, key=lambda t: t[0])[0],
-                  max(keys, key=lambda t: t[1])[1])
+    print("Version: {}".format(version))
 
-        matrix = [[0 for _ in range(length[1])] for _ in range(length[0])]
-
-        for index, value in var.items():
-            x = index[0] - 1
-            y = index[1] - 1
-
-            matrix[x][y] = value
-
-        return np.matrix(matrix)
-    else:
-        raise NotImplemented
-
-
-def pretty_print(results, model):
-    print("Problem")
-    print(results['Problem'])
     print("Solver")
-    print(results['Solver'])
+    print("time: {}".format(solver['time']))
+    print("termination condition: {}".format(solver['termination_condition']))
 
     print("Solution")
-    solution = results['Solution'][0]
 
-    # Get the objective and print it
     print("Objective")
-    print("\ttotal_cost: {}".format(solution['Objective']['Total_Cost']['Value']))
-
-    # Print the variables
-    variables = {k: v for k, v in model.__dict__.items()
-                 if isinstance(v, Var)}
-    variables = OrderedDict(sorted(variables.items()))
+    objectives = results['solution']['objective']
+    objectives = OrderedDict(sorted(objectives.items()))
+    for name, value in objectives.items():
+        print("\t{}: {}".format(name, value))
 
     print("Variables")
+
+    variables = results['solution']['variables']
+    variables = OrderedDict(sorted(variables.items()))
     for name, value in variables.items():
-        if hasattr(value, 'value'):
-            value = value.value
-        else:
-            value = value.get_values()
+        if isinstance(value, list):
+            value = np.array(value)
 
-        if isinstance(value, dict):
-            value = to_matrix(value)
-
-            print("\n{}:\n{}".format(name, value))
-            # for x, row in enumerate(value):
-            #     print("\t\t{}: {}".format(x, row))
-        else:
-            print("\n{}: {}".format(name, value))
+        print("\n{}: \n{}".format(name, value))
 
 
 def main():
@@ -76,14 +43,11 @@ def main():
 
     results = model.solve()
 
-    np.set_printoptions(linewidth=1000, suppress=True)
-
-    # Pretty bad, but it is temporary
-    pretty_print(results, model._model)
+    pretty_print(results)
 
     with open(settings["output_file"], 'w') as f:
         with redirect_stdout(f):
-            pretty_print(results, model._model)
+            pretty_print(results)
 
 
 if __name__ == "__main__":
