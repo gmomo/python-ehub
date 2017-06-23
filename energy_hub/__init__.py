d900d5943014a04d6421eef429c506f46e776bd1
Grey Hill <infogreytech@gmail.com>
2017-06-23 09:34:15 -0700

    Add capacities as variables to model

--- energy_hub/__init__.py
+++ energy_hub/__init__.py
@@ -6,4 +6,5 @@ from energy_hub.time_series import TimeSeries
 from energy_hub.storage import Storage
 from energy_hub.converter import Converter
 from energy_hub.stream import Stream
+from energy_hub.capacity import Capacity
 from energy_hub.ehub_model import EHubModel
